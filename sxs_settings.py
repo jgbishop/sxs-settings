e756403288858b6694525b52011564038808a7c8
Jonah Bishop <git@borngeek.com>
2017-12-27 22:51:43 -0500

    PEP-8-ify our code
    This plugin wasn't PEP-8 compliant, and I'm making it so.

--- sxs_settings.py
+++ sxs_settings.py
@@ -1,120 +1,134 @@
-import sublime, sublime_plugin
 import re
+import sublime
+import sublime_plugin
 
 _WINDOW_ID = None
 
-def closeWindowIfNeeded(self):
-	global _WINDOW_ID
 
-	if _WINDOW_ID is not None:
-		for i,w in enumerate(sublime.windows()):
-			if w.id() == _WINDOW_ID:
-				w.run_command("close_window")
-				_WINDOW_ID = None
-				return True
-	return False
+def close_window_if_needed(self):
+    global _WINDOW_ID
 
-def getSetting(pref, default):
-	return sublime.load_settings('sxs_settings.sublime-settings').get(pref, default)
+    if _WINDOW_ID is not None:
+        for i, w in enumerate(sublime.windows()):
+            if w.id() == _WINDOW_ID:
+                w.run_command("close_window")
+                _WINDOW_ID = None
+                return True
+    return False
 
-def openWindow(self, leftPath):
-	global _WINDOW_ID
 
-	# Self in this context is the active window
-	self.run_command("new_window")
-	new_window = sublime.active_window()
-	_WINDOW_ID = new_window.id()
+def get_setting(pref, default):
+    return sublime.load_settings('sxs_settings.sublime-settings').get(pref, default)
 
-	if getSetting('display_using_rows', False) == True:
-		new_window.set_layout({
-		    "cols": [0, 1],
-		    "rows": [0, 0.5, 1],
-		    "cells": [[0, 0, 1, 1], [0, 1, 1, 2]]
-		})
-	else:
-		new_window.set_layout({
-		    "cols": [0, 0.5, 1],
-		    "rows": [0, 1],
-		    "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
-		})
 
-	if getSetting('open_in_distraction_free', False) == True:
-		new_window.run_command('toggle_distraction_free')
-		new_window.run_command('toggle_tabs')
+def open_window(self, left_path):
+    global _WINDOW_ID
 
-	lastSlash = leftPath.rfind("/")
-	rightPath = leftPath[(lastSlash+1):] # Extract the filename
+    # Self in this context is the active window
+    self.run_command("new_window")
+    new_window = sublime.active_window()
+    _WINDOW_ID = new_window.id()
 
-	# If we're opening a .sublime-keymap file, the right pane should always open
-	# to "Default ({platform}).sublime-keymap" since that's where keys should be
-	# put.
-	if re.search(r"\.sublime-keymap", leftPath):
-		platform = "Windows" # Assume this to start (evil, I know)
-		plat = sublime.platform()
-		if plat == "linux":
-			platform = "Linux"
-		elif plat == "osx":
-			platform = "OSX"
+    if get_setting('display_using_rows', False):
+        new_window.set_layout({
+            "cols": [0, 1],
+            "rows": [0, 0.5, 1],
+            "cells": [[0, 0, 1, 1], [0, 1, 1, 2]]
+        })
+    else:
+        new_window.set_layout({
+            "cols": [0, 0.5, 1],
+            "rows": [0, 1],
+            "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
+        })
 
-		rightPath = "Default (" + platform + ").sublime-keymap"
+    if get_setting('open_in_distraction_free', False):
+        new_window.run_command('toggle_distraction_free')
+        new_window.run_command('toggle_tabs')
 
-	# Test to see if we are opening a platform-specific settings file. If so,
-	# strip the platform specific portion of the filename (platform-specific
-	# files are ignored in the User directory)
-	elif re.search(r" \((?:Linux|OSX|Windows)\).sublime-settings", leftPath):
-		rightPath = re.sub(r" \((?:Linux|OSX|Windows)\)", "", rightPath)
+    last_slash = left_path.rfind("/")
+    right_path = left_path[(last_slash + 1):]  # Extract the filename
 
-	rightContents = "{\n\t$0\n}\n" # Default to object notation for sublime-settings files
-	if re.search(r"\.sublime-keymap", leftPath):
-		rightContents = "[\n\t$0\n]\n"; # Use array notation for sublime-keymap files
+    # If we're opening a .sublime-keymap file, the right pane should always open
+    # to "Default ({platform}).sublime-keymap" since that's where keys should be
+    # put.
+    if re.search(r"\.sublime-keymap", left_path):
+        platform = "Windows"  # Assume this to start (evil, I know)
+        plat = sublime.platform()
+        if plat == "linux":
+            platform = "Linux"
+        elif plat == "osx":
+            platform = "OSX"
 
-	new_window.run_command("open_file", {'file': "${packages}/" + leftPath})
-	new_window.run_command("open_file", {'file': "${packages}/User/" + rightPath, 'contents': rightContents })
-	new_window.set_view_index(new_window.active_view(), 1, 0)
+        right_path = "Default (" + platform + ").sublime-keymap"
 
-class sxsSettingsCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		if closeWindowIfNeeded(self) == True:
-			return
-		openWindow(self.window, "Default/Preferences.sublime-settings")
+    # Test to see if we are opening a platform-specific settings file. If so,
+    # strip the platform specific portion of the filename (platform-specific
+    # files are ignored in the User directory)
+    elif re.search(r" \((?:Linux|OSX|Windows)\).sublime-settings", left_path):
+        right_path = re.sub(r" \((?:Linux|OSX|Windows)\)", "", right_path)
 
-class sxsKeyBindingsCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		if closeWindowIfNeeded(self) == True:
-			return
-		openWindow(self.window, "Default/Default ($platform).sublime-keymap")
+    right_contents = "{\n\t$0\n}\n"  # Default to object notation for sublime-settings files
+    if re.search(r"\.sublime-keymap", left_path):
+        right_contents = "[\n\t$0\n]\n"  # Use array notation for sublime-keymap files
 
-class sxsSelectFileCommand(sublime_plugin.WindowCommand):
-	fileList = []
+    new_window.run_command("open_file", {'file': "${packages}/" + left_path})
+    new_window.run_command(
+        "open_file", {
+            'file': "${packages}/User/" + right_path,
+            'contents': right_contents
+        }
+    )
+    new_window.set_view_index(new_window.active_view(), 1, 0)
 
-	def run(self):
-		if closeWindowIfNeeded(self) == True:
-			return
 
-		self.fileList[:] = [] # Clear our cache
+class SxsSettingsCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        if close_window_if_needed(self):
+            return
+        open_window(self.window, "Default/Preferences.sublime-settings")
 
-		settingsList = sublime.find_resources("*.sublime-settings")
-		keymapList = sublime.find_resources("*.sublime-keymap")
 
-		tempList = settingsList + keymapList
+class SxsKeyBindingsCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        if close_window_if_needed(self):
+            return
+        open_window(self.window, "Default/Default ($platform).sublime-keymap")
 
-		for i, item in enumerate(tempList):
-			tempItem = re.sub(r"^Packages/", "", item)
-			if re.match(r"User/", tempItem):
-				continue # Ignore anything we find in the User directory (those will get treated as "right pane" files)
-			else:
-				self.fileList.append(tempItem)
 
-		self.fileList.sort()
-		self.window.show_quick_panel(self.fileList, self.onDone)
+class SxsSelectFileCommand(sublime_plugin.WindowCommand):
+    file_list = []
 
-	def is_enabled(self):
-		return (int(sublime.version()) >= 3000)
+    def run(self):
+        if close_window_if_needed(self):
+            return
 
-	def is_visible(self):
-		return (int(sublime.version()) >= 3000)
+        self.file_list[:] = []  # Clear our cache
 
-	def onDone(self, index):
-		if index == -1:
-			return
-		openWindow(self.window, self.fileList[index])
+        settings_list = sublime.find_resources("*.sublime-settings")
+        keymap_list = sublime.find_resources("*.sublime-keymap")
+
+        templist = settings_list + keymap_list
+
+        for i, item in enumerate(templist):
+            temp_item = re.sub(r"^Packages/", "", item)
+            if re.match(r"User/", temp_item):
+                # Ignore anything we find in the User directory
+                # (those will get treated as "right pane" files)
+                continue
+            else:
+                self.file_list.append(temp_item)
+
+        self.file_list.sort()
+        self.window.show_quick_panel(self.file_list, self.on_done)
+
+    def is_enabled(self):
+        return (int(sublime.version()) >= 3000)
+
+    def is_visible(self):
+        return (int(sublime.version()) >= 3000)
+
+    def on_done(self, index):
+        if index == -1:
+            return
+        open_window(self.window, self.file_list[index])
